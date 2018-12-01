942da2777a1cbe13e29cbc59cfb8d6a4315c55e2
Unknown <clydecaramba@gmail.com>
2018-12-02 02:08:04 +0500

    First commit

---
+++ domains/localhost/administrator/logs/com_virtuemart.log.php
@@ -0,0 +1,27 @@
+#
+#<?php die("Forbidden."); ?>
+
+2018-08-02 09:11:29 ERROR vmError: vmPdf: For the pdf, you must install the tcpdf library at F:\OSPanel\domains\localhost\libraries	cpdf
+2018-08-02 09:22:21 ERROR vmError: Couldnt create thumb, file not found F:\OSPanel\domains\localhost\images\stories\virtuemart\category\25.jpg
+2018-08-02 09:22:21 ERROR vmError: Файл F:\OSPanel\domains\localhost/images/stories/virtuemart/category/25.jpg не найден
+2018-08-02 09:22:21 ERROR vmError: Couldnt create thumb, file not found F:\OSPanel\domains\localhost\images\stories\virtuemart\category\25.jpg
+2018-08-02 09:22:31 ERROR vmError: Couldnt create thumb, file not found F:\OSPanel\domains\localhost\images\stories\virtuemart\category\25.jpg
+2018-08-02 09:22:31 ERROR vmError: Файл F:\OSPanel\domains\localhost/images/stories/virtuemart/category/25.jpg не найден
+2018-08-02 09:22:31 ERROR vmError: Couldnt create thumb, file not found F:\OSPanel\domains\localhost\images\stories\virtuemart\category\25.jpg
+2018-08-02 09:22:36 ERROR vmError: Couldnt create thumb, file not found F:\OSPanel\domains\localhost\images\stories\virtuemart\category\25.jpg
+2018-08-02 09:22:36 ERROR vmError: Файл F:\OSPanel\domains\localhost/images/stories/virtuemart/category/25.jpg не найден
+2018-08-02 09:22:36 ERROR vmError: Couldnt create thumb, file not found F:\OSPanel\domains\localhost\images\stories\virtuemart\category\25.jpg
+2018-08-02 09:23:03 ERROR vmError: Couldnt create thumb, file not found F:\OSPanel\domains\localhost\images\stories\virtuemart\category\25.jpg
+2018-08-02 09:23:03 ERROR vmError: Файл F:\OSPanel\domains\localhost/images/stories/virtuemart/category/25.jpg не найден
+2018-08-02 09:23:03 ERROR vmError: Couldnt create thumb, file not found F:\OSPanel\domains\localhost\images\stories\virtuemart\category\25.jpg
+2018-08-02 09:24:03 ERROR vmError: Couldnt create thumb, file not found F:\OSPanel\domains\localhost\images\stories\virtuemart\category\5.jpg
+2018-08-02 09:24:03 ERROR vmError: Файл F:\OSPanel\domains\localhost/images/stories/virtuemart/category/5.jpg не найден
+2018-08-02 09:24:03 ERROR vmError: Couldnt create thumb, file not found F:\OSPanel\domains\localhost\images\stories\virtuemart\category\5.jpg
+2018-08-02 09:46:25 ERROR vmError: vmPdf: For the pdf, you must install the tcpdf library at F:\OSPanel\domains\localhost\libraries	cpdf
+2018-08-02 09:47:17 ERROR vmError: vmPdf: For the pdf, you must install the tcpdf library at F:\OSPanel\domains\localhost\libraries	cpdf
+2018-08-02 09:47:28 ERROR vmError: vmPdf: For the pdf, you must install the tcpdf library at F:\OSPanel\domains\localhost\libraries	cpdf
+2018-08-02 11:09:11 ERROR vmError: vmPdf: For the pdf, you must install the tcpdf library at F:\OSPanel\domains\localhost\libraries	cpdf
+2018-08-04 09:52:55 ERROR vmError: vmPdf: For the pdf, you must install the tcpdf library at F:\OSPanel\domains\localhost\libraries	cpdf
+2018-08-04 10:06:28 ERROR vmError: Invalid media, image type does not fit to extension TMX-2050_1.jpg 3!=jpg
+2018-08-04 10:06:40 ERROR vmError: Invalid media, image type does not fit to extension TMX-2050_1.jpg 3!=jpg
+2018-08-04 10:07:11 ERROR vmError: Invalid media, image type does not fit to extension TMX-2050_1.jpg 3!=jpg
---
+++ domains/localhost/administrator/logs/error.php
@@ -0,0 +1,17 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2018-07-30 11:18:30 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2018-07-30T11:18:30+00:00	INFO 127.0.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
+2018-08-02T14:29:44+00:00	INFO 127.0.0.1	cookiefailure	Пустой пароль не допускается
+2018-08-02T14:30:47+00:00	INFO 127.0.0.1	joomlafailure	Имя пользователя и пароль не совпадают или у вас ещё нет учётной записи на сайте
+2018-08-02T15:08:28+00:00	INFO 127.0.0.1	cookiefailure	Пустой пароль не допускается
+2018-08-07T14:37:41+00:00	INFO 127.0.0.1	cookiefailure	Пустой пароль не допускается
+2018-08-08T04:21:07+00:00	INFO 127.0.0.1	cookiefailure	Пустой пароль не допускается
+2018-08-08T04:21:15+00:00	INFO 127.0.0.1	updater	Loading information from update site #1 with name "Joomla! Core" and URL https://update.joomla.org/core/list.xml took 3.07 seconds
+2018-08-08T04:30:11+00:00	INFO 127.0.0.1	cookiefailure	Пустой пароль не допускается
+2018-08-08T04:30:21+00:00	INFO 127.0.0.1	updater	Loading information from update site #1 with name "Joomla! Core" and URL https://update.joomla.org/core/list.xml took 4.29 seconds
+2018-08-08T17:22:09+00:00	INFO 127.0.0.1	cookiefailure	Пустой пароль не допускается
+2018-08-08T17:22:19+00:00	INFO 127.0.0.1	updater	Loading information from update site #1 with name "Joomla! Core" and URL https://update.joomla.org/core/list.xml took 1.05 seconds
